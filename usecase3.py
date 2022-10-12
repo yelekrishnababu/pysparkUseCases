d801120490f74bd8ebd1e6da8c24b7306bb6ce5b
yelekrishnababu <krishnaktsmcl@gmail.com>
2022-10-12 12:46:52 +0530

    first commit

--- usecase3.py
+++ usecase3.py
@@ -1,6 +1,23 @@
-from openpyxl import Workbook
-from openpyxl.reader.excel import load_workbook
-wb=Workbook()
-filePath="/home/ky2910/PycharmProjects/pysparkUseCases/inputs/test.xlsx"
-wb=load_workbook(filePath)
-print(wb.sheetnames)
+from pyspark.sql import SparkSession, DataFrameWriter
+spark = SparkSession.builder.master("local[1]").appName('SparkByExamples.com').getOrCreate()
+df = spark.read.csv("/home/ky2910/PycharmProjects/pysparkUseCases/file1csv.csv.gz", header=True)
+jdbcHostname = "w3.training5.modak.com"
+jdbcPort = "5432"
+jdbcDatabase = "training"
+properties = {
+ "user" : "mt4038",
+ "password" : "mt4038@m02y22",
+"driver": "org.postgresql.Driver"
+}
+url = "jdbc:postgresql://{0}:{1}/database={2}".format(jdbcHostname,jdbcPort,jdbcDatabase)
+# df.write.format("jdbc").mode("overwrite").option("url","jdbc:postgresql://w3.training5.modak.com:5432/training").option(
+#  "driver", "org.postgresql.Driver").option("dbtable", "testKY2910").option("user","mt4038").option("password", "mt4038@m02y22").save()
+df.write.jdbc(url=url, table= "test_kY2910", mode ="overwrite", properties = properties)
+# myfinaldf.write \
+#     .format("jdbc") \
+#     .option("url", "jdbc:postgresql://w3.training5.modak.com:5432/training") \
+#     .option("dbtable", "testKY2910") \
+#     .option("user", "mt4038") \
+#     .option("password", "mt4038@m02y22") \
+#     .option("driver", "org.postgresql.Driver") \
+#     .save()
